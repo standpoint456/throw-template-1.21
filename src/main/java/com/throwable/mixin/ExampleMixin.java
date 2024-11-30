c68bd136f3db2d999bc2ff45a96c46ddd4d6d28a
IntelliFish123 <vlad.robert.juganaru@gmail.com>
2024-11-30 17:18:24 +0200

    update

--- src/main/java/com/throwable/mixin/ExampleMixin.java
+++ src/main/java/com/throwable/mixin/ExampleMixin.java
@@ -1,5 +1,7 @@
 package com.throwable.mixin;
 
+import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,45 +10,44 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static com.throwable.Throw.FuseTime;
 
-import net.minecraft.util.Mth;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.item.PrimedTnt;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.TntEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.WorldProperties;
 
 @Mixin(Item.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "use")
-	private void init(Level world, Player user, InteractionHand hand,
-					  CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
-		if(!world.isClientSide){
-			if(user.getItemInHand(hand).getItem()== Items.TNT) {
-				ItemStack itemStack = user.getItemInHand(hand);
-				PrimedTnt tnt = new PrimedTnt(world, user.getX(), user.getY(), user.getZ(), user);
-				setVelocity(tnt, user, user.getXRot(), user.getYRot());
-				world.addFreshEntity(tnt);
-				tnt.setFuse(world.getGameRules().getRule(FuseTime).get());
-				itemStack.consume(1, user);
+	private void init(World world, PlayerEntity user, Hand hand,
+					  CallbackInfoReturnable<ActionResult> cir) {
+		if(!world.isClient){
+			if(user.getStackInHand(hand).getItem()== Items.TNT) {
+				ItemStack itemStack = user.getStackInHand(hand);
+				TntEntity tnt = new TntEntity(world, user.getX(), user.getY(), user.getZ(), user);
+				setVelocity(tnt, user, user.getPitch(), user.getYaw());
+				world.spawnEntity(tnt);
+				tnt.setFuse(world.getGameRules().get(FuseTime).get());
+				itemStack.decrementUnlessCreative(1, user);
 			}
 		}
 	}
 
 	@Unique
-	void setVelocity(PrimedTnt tnt, Entity shooter, float pitch, float yaw) {
-		float f = -Mth.sin(yaw * ((float)Math.PI / 180))
-				* Mth.cos(pitch * ((float)Math.PI / 180));
-		float g = -Mth.sin((pitch + (float) 2) *
+	void setVelocity(TntEntity tnt, Entity shooter, float pitch, float yaw) {
+		float f = -MathHelper.sin(yaw * ((float)Math.PI / 180))
+				* MathHelper.cos(pitch * ((float)Math.PI / 180));
+		float g = -MathHelper.sin((pitch + (float) 2) *
 				((float)Math.PI / 180));
-		float h = Mth.cos(yaw * ((float)Math.PI / 180))
-				* Mth.cos(pitch * ((float)Math.PI / 180));
-		tnt.setDeltaMovement(f, g, h);
-		Vec3 vec3d = shooter.getKnownMovement();
-		tnt.setDeltaMovement(tnt.getDeltaMovement().add(vec3d.x,vec3d.y, vec3d.z));
+		float h = MathHelper.cos(yaw * ((float)Math.PI / 180))
+				* MathHelper.cos(pitch * ((float)Math.PI / 180));
+		tnt.setVelocity(f, g, h);
+		Vec3d vec3d = shooter.getMovement();
+		tnt.setVelocity(tnt.getVelocity().add(vec3d.x,vec3d.y, vec3d.z));
 	}
 }
